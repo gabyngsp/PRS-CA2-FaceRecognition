f3ce1119fcda9c0199aea4b4469e0bba84613288
tealeeseng <tealeeseng@gmail.com>
2019-09-06 16:19:47 +0800

    Prepare data for training

--- poc/videoToImages.py
+++ poc/videoToImages.py
@@ -1,17 +1,23 @@
+import os
+
 import cv2
 
-def leesengVideoToImages():
-    vidcap = cv2.VideoCapture('rawData/LeeSeng/VID_20190902_091449.mp4')
+
+def videoToImages(className, filename):
+    print('Converting ', filename)
+    vidcap = cv2.VideoCapture(os.path.join('rawData', className, filename))
     success, image = vidcap.read()
     count = 0
     while success:
         # rows, cols, channels = image.shape
-        # M =cv2.getRotationMatrix2D((cols/2, rows/2),90,1)
-        # nImage = cv2.warpAffine( image, M, (rows, cols))
-        image = cv2.transpose(image)
-        image = cv2.flip(image, 0)
 
-        cv2.imwrite('data/LeeSeng/frame%d.jpg' % count, image)
+        if className in ('XiaoYan', 'GabyNg'):
+            image = cv2.transpose(image)
+        elif className in ('LeeSeng'):
+            image = cv2.transpose(image)
+            image = cv2.flip(image, 0)
+
+        cv2.imwrite(os.path.join('data', className, filename) + '-%d.jpg' % count, image)
         success, image = vidcap.read()
         if success:
             print('.', end='')
@@ -19,98 +25,31 @@ def leesengVideoToImages():
             print('X', end='')
 
         # print('Read new frame:', success)
-        count+=1
+        count += 1
 
-def leeseng2VideoToImages():
-    vidcap = cv2.VideoCapture('rawData/LeeSeng/VID_20190905_204615.mp4')
-    success, image = vidcap.read()
-    count = 0
-    while success:
-        # rows, cols, channels = image.shape
-        # M =cv2.getRotationMatrix2D((cols/2, rows/2),90,1)
-        # nImage = cv2.warpAffine( image, M, (rows, cols))
-        image = cv2.transpose(image)
-        image = cv2.flip(image, 0)
-
-        cv2.imwrite('data/LeeSeng/fframe%d.jpg' % count, image)
-        success, image = vidcap.read()
-        if success:
-            print('.', end='')
-        else:
-            print('X', end='')
-
-        # print('Read new frame:', success)
-        count+=1
-
-def gabyNgVideoToImages():
-    vidcap = cv2.VideoCapture('rawData/GabyNg/20190902_124712.mp4')
-    success, image = vidcap.read()
-    count = 0
-    while success:
-        # rows, cols, channels = image.shape
-        # M =cv2.getRotationMatrix2D((cols/2, rows/2),90,1)
-        # nImage = cv2.warpAffine( image, M, (rows, cols))
-        image = cv2.transpose(image)
-        # image = cv2.flip(image, 0)
-
-        cv2.imwrite('data/GabyNg/bframe%d.jpg' % count, image)
-        success, image = vidcap.read()
-        if success:
-            print('.', end='')
-        else:
-            print('X', end='')
-
-        # print('Read new frame:', success)
-        count+=1
-
-def gabyNg2VideoToImages():
-    vidcap = cv2.VideoCapture('rawData/GabyNg/20190902_124535.mp4')
-    success, image = vidcap.read()
-    count = 0
-    while success:
-        # rows, cols, channels = image.shape
-        # M =cv2.getRotationMatrix2D((cols/2, rows/2),90,1)
-        # nImage = cv2.warpAffine( image, M, (rows, cols))
-        image = cv2.transpose(image)
-        # image = cv2.flip(image, 0)
-
-        cv2.imwrite('data/GabyNg/cframe%d.jpg' % count, image)
-        success, image = vidcap.read()
-        if success:
-            print('.', end='')
-        else:
-            print('X', end='')
-
-        # print('Read new frame:', success)
-        count+=1
-
-def xiaoYanVideoToImages():
-    vidcap = cv2.VideoCapture('rawData/XiaoYan/IMG_4705.MOV')
-    success, image = vidcap.read()
-    count = 0
-    while success:
-        # rows, cols, channels = image.shape
-        # M =cv2.getRotationMatrix2D((cols/2, rows/2),90,1)
-        # nImage = cv2.warpAffine( image, M, (rows, cols))
-        image = cv2.transpose(image)
-        # image = cv2.flip(image, 0)
-
-        cv2.imwrite('data/XiaoYan/xframe%d.jpg' % count, image)
-        success, image = vidcap.read()
-        if success:
-            print('.', end='')
-        else:
-            print('X', end='')
-
-        # print('Read new frame:', success)
-        count+=1
 
 def main():
+    print('Creates folder.')
+    os.makedirs(os.path.join('data', 'LeeSeng'))
+    os.makedirs(os.path.join('data', 'XiaoYan'))
+    os.makedirs(os.path.join('data', 'GabyNg'))
+
+    print('Converting Videos to images...')
     # leesengVideoToImages()
-    # gabyNgVideoToImages()
+    # leeseng2VideoToImages()
+    videoToImages('LeeSeng', 'VID_20190902_091449.mp4')
+    videoToImages('LeeSeng', 'VID_20190905_204615.mp4')
+
     # xiaoYanVideoToImages()
+    videoToImages('XiaoYan', 'IMG_4747.MOV')
+    videoToImages('XiaoYan', 'IMG_4705.MOV')
+
+    # gabyNgVideoToImages()
     # gabyNg2VideoToImages()
-    leeseng2VideoToImages()
+    videoToImages('GabyNg', '20190902_124535.mp4')
+    # videoToImages('GabyNg', '20190902_124712.mp4')
+    print('== END ==')
+
 
 if __name__ == '__main__':
     main()
