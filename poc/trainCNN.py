63b38f533dcbd89e2b3f42e7d34df1980488c451
tealeeseng <tealeeseng@gmail.com>
2019-09-08 20:58:33 +0800

    without face clipping and still works.

--- poc/trainCNN.py
+++ poc/trainCNN.py
@@ -194,24 +194,26 @@ def createModel(target_size=(128, 128)):
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
+    ## leeseng: deeper block that make model training converges.
     model.add(Conv2D(256, (3, 3)))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
+    #
+    # model.add(Conv2D(256, (3, 3)))
+    # model.add(Activation('relu'))
+    # model.add(MaxPooling2D(pool_size=(2, 2)))
+    # model.add(Dropout(0.25))
+    #
+    # model.add(Conv2D(512, (3, 3)))
+    # model.add(Activation('relu'))
 
-    model.add(Conv2D(256, (3, 3)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-
-    model.add(Conv2D(512, (3, 3)))
-    model.add(Activation('relu'))
     # model.add(MaxPooling2D(pool_size=(2, 2)))
     # model.add(Conv2D(512, (3, 3)))
     # model.add(Activation('relu'))
 
     model.add(Flatten())
-    model.add(Dense(128))
+    model.add(Dense(32))
     model.add(Activation('relu'))
     model.add(Dropout(0.5))
     model.add(Dense(3, activation='softmax'))
--- poc/videoToImages.py
+++ poc/videoToImages.py
@@ -18,10 +18,16 @@ def videoToImages(className, filename):
             image = cv2.transpose(image)
             image = cv2.flip(image, 0)
 
-        fls = face_recognition.face_locations(image)
-        for f in fls:
-            cv2.imwrite(os.path.join('data', className, filename) + '-%d.jpg' % count, image[f[0]:f[2], f[3]:f[1]])
-            count += 1
+
+        ## with face clipping
+        # fls = face_recognition.face_locations(image)
+        # for f in fls:
+        #     cv2.imwrite(os.path.join('data', className, filename) + '-%d.jpg' % count, image[f[0]:f[2], f[3]:f[1]])
+        #     count += 1
+
+        # without face clipping
+        cv2.imwrite(os.path.join('data', className, filename) + '-%d.jpg' % count, image)
+        count += 1
 
         success, image = vidcap.read()
         # if success:
@@ -43,11 +49,11 @@ def main():
     print('Converting Videos to images...')
     # videoToImages('LeeSeng', 'VID_20190902_091449.mp4')
     # videoToImages('LeeSeng', 'VID_20190905_204615.mp4')
-    videoToImages('LeeSeng', 'VID_20190908_145635.mp4')
+    # videoToImages('LeeSeng', 'VID_20190908_145635.mp4')
 
-    # videoToImages('XiaoYan', 'IMG_4749.MOV')
+    videoToImages('XiaoYan', 'IMG_4749.MOV')
     # videoToImages('XiaoYan', 'IMG_4747.MOV')
-    # videoToImages('XiaoYan', 'IMG_4705.MOV')
+    videoToImages('XiaoYan', 'IMG_4705.MOV')
 
     # videoToImages('GabyNg', '20190902_124535.mp4')
     # videoToImages('GabyNg', '20190902_124712.mp4')
